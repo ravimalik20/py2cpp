6639846fe95b3840322b0f33d50e299effacb364
Ravi Malik <ravimalik2364@gmail.com>
2013-08-16 21:21:28 +0530

    Added the module for generic variables.

---
+++ variable2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+
+#define GET(a) ((a.t==1)?(a.v.d):\
+		((a.t==2)?(a.v.ld):\
+		((a.t==3)?(a.v.f):\
+		((a.t==4)?(a.v.lf):\
+		((a.t==5)?(a.v.c):\
+		('\0')\
+		)))))
+
+union __var
+{	int d;
+	long ld;
+	float f;
+	double lf;
+	char c;
+};
+
+class Var
+{	public:
+		union __var v;
+		int t;
+
+		void put(int a)
+		{	v.d=a;
+			t=1;
+		}
+		void put(long a)
+		{	v.ld=a;
+			t=2;
+		}
+		void put(float a)
+		{	v.f=a;
+			t=3;
+		}
+		void put(double a)
+		{	v.lf=a;
+			t=4;
+		}
+		void put(char a)
+		{	v.c=a;
+			t=5;
+		}
+};
+
+int main(void)
+{	Var v;
+
+	int a=25;
+
+	v.put(12);
+	
+	cout<<GET(v)<<"\n";
+
+	return 0;
+}
