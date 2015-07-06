142b39aab905704b095ab56238696e5e60266ea9
Ravi Malik <ravimalik2364@gmail.com>
2015-07-06 13:27:24 +0530

    Added sample benchmark input files
    Added a bunch of files to perform benchmark on. All the files are python scripts
    , fairly simple and have all the inputs hardcoded into the source code.
    
    Updated .gitignore to exclude libreoffice lock files on the csv.

---
+++ sample_benchmark_inputs/factorial.py
@@ -0,0 +1,12 @@
+n=5
+
+if n<0:
+	print "Cannot generate factorial of negative numbers."
+
+else:
+	a=1
+	while n>0:
+		a=a*n
+		n=n-1
+
+	print a
---
+++ sample_benchmark_inputs/prime_4.py
@@ -0,0 +1,17 @@
+n=175
+
+a=2
+
+b=1
+
+while a*a<=n:
+	if n%a==0:
+		print "Not Prime"
+
+		b=0
+
+		break
+	a=a+1
+
+if b==1:
+	print "Prime"
